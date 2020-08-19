83d883ed78b5fcd35bb822284f5a593950439f87
Localboot <you@example.com>
2020-08-19 14:27:37 +1200

    myreactspa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './View.js';
+import View from './View';
 import './App.css';
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   setActiveView = (view) => {
-      this.setState({activeView:View})
+      this.setState({activeView:view})
   }
   
   render(){
@@ -24,38 +24,57 @@ class App extends Component {
 
       <div className="app">
 
-          <View viewName="Home" activeView={this.state.activeView}className="color1">
-              <div className="header"><i onClick={() => this.setActiveView('Home')} className="fas fa-bars"></i></div>
-              <div className="main">
-                Home
-              </div>
-          </View>
+        <View viewName="home" activeView={this.state.activeView} className="color1">
+          <div className="header"><i onClick={() => this.setActiveView('cart')} className="fas fa-bars"></i></div>
+          <div className="main">
+            <p onClick={() => this.setActiveView('login')}></p>
+            <h1>Home</h1>
+            <h1 onClick={() => this.setActiveView('order')}>Shop</h1>
+            <h3 onClick={() => this.setActiveView('contact1')}>Contact us</h3>
+          </div>
+        </View>
 
-      <View viewName="Cart" activeView={this.state.activeView}className="color2">
-        <div className="header"><i onClick={() => this.setActiveView('Home')}className="fas fa-times"></i></div>
+        <View viewName="cart" activeView={this.state.activeView} className="color2">
+          <div className="header"><i onClick={() => this.setActiveView('home')} className="fas fa-times"></i></div>
+          <div className="main">
+            <p onClick={() => this.setActiveView('home')}>go to back to home</p>
+            <h1 onClick={() => this.setActiveView('order')}>Shop</h1>
+            Cart
+        </div>
+        </View>
+
+      <View viewName="order" activeView={this.state.activeView}className="color3">
+        <div className="header"><i onClick={() => this.setActiveView('home')}className="fas fa-times"></i></div>
         <div className="main">
-          Cart
+          <p onClick={() => this.setActiveView('home')}>go to back to home</p>
+          <h2> Order</h2>
+          <p onClick={() => this.setActiveView('contact1')}>Find us</p>
         </div>
       </View>
 
-      <view viewName="Order" activeView={this.state.activeView}className="color3">
-        <div className="header"><i onClick={() => this.setActiveView('Home')}className="fas fa-times"></i></div>
-        <div className="main">
-          Order
-        </div>
-      </view>
-
-      <View viewName="Login" activeView={this.state.activeView}className="color4">
-        <div className="header"><i onClick={() => this.setActiveView('Home')}className="fas fa-times"></i></div>
+      <View viewName="login" activeView={this.state.activeView}className="color4">
+        <div className="header"><i onClick={() => this.setActiveView('home')}className="fas fa-times"></i></div>
         <div className="main">
           Login
         </div>
       </View>
 
-      <View viewName="Contact" activeView={this.state.activeView}className="color5">
-        <div className="header"><i onClick={() => this.setActiveView('Home')}className="fas fa-times"></i></div>
+
+      <View viewName="login2" activeView={this.state.activeView}className="color4">
+        <div className="header"><i onClick={() => this.setActiveView('home')}className="fas fa-times"></i></div>
         <div className="main">
-          Contact
+          Login 2
+        </div>
+      </View>
+
+      <View viewName="contact1" activeView={this.state.activeView}className="color1">
+        <div className="header"><i onClick={() => this.setActiveView('home')}className="fas fa-times"></i></div>
+        <div className="main">
+          <p onClick={() => this.setActiveView('home')}>go to back to home</p>
+          <h3>Contact</h3>
+          <p>Stacy Lee Ghin<br/>
+          stacyleeghin@gmail.com<br/>
+          mob 021977920</p>
         </div>
       </View>
   
